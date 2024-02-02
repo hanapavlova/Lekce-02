0888a66398669b78c8bc401a179287dd92a65d3f
Hana Pavlova <hanapavlova7@gmail.com>
2024-02-02 13:12:43 +0100

    ukoly komplet

--- script.js
+++ script.js
@@ -5,8 +5,16 @@ let cislo = Math.floor((Math.random()*6)+1);
 document.body.innerHTML += cislo;
 
 //2. úkol
-let cena = 12
-let pocetListku = 174*15
-let prijem = cena*pocetListku
+let cena = 12;
+let pocetListku = 174*15;
+let prijem = cena*pocetListku;
 
-document.body.innerHTML += "<p>Měsíční příjem divadla je " + prijem + " EUR.</p>"
+document.body.innerHTML += "<p>Měsíční příjem divadla je " + prijem + " EUR.</p>";
+
+let pocetStudent = pocetListku*0.4;
+let pocetDospely = pocetListku*0.6;
+let cenaStudent = 12*0.65;
+
+let prijemSleva = cena*pocetDospely + cenaStudent*pocetStudent;
+
+document.body.innerHTML += "<p>Měsíční příjem divadla po zapocitani studentskych slev je " + prijemSleva + " EUR.</p>";
