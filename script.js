f406096f24f180406d67e71366ad3d33d8329694
Hana Pavlova <hanapavlova7@gmail.com>
2024-02-02 13:07:26 +0100

    ukoly

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 document.body.innerHTML += "<p> Ahoj Verunko!!!</p>";
+
+//1. úkol
+let cislo = Math.floor((Math.random()*6)+1);
+document.body.innerHTML += cislo;
+
+//2. úkol
+let cena = 12
+let pocetListku = 174*15
+let prijem = cena*pocetListku
+
+document.body.innerHTML += "<p>Měsíční příjem divadla je " + prijem + " EUR.</p>"
